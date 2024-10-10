9cc70ec1dee8e55428e27f82b365a630a615e814
Byord-ml <byordkim@gmail.com>
2024-10-10 16:57:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import os
 import google.generativeai as genai
 
 # Configure the API key for Google Generative AI
-genai.configure(api_key='AIzaSyBT4qz_pSZrB8yCqfQeU7jm1YhQYumhDQw')
+genai.configure(api_key='')
 
 
 def persona(input):
